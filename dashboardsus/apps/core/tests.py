5009f4ae1faeb13fc7f56ccabcdec00f0e87b564
Wendell P. Barreto <wendellp.barreto@gmail.com>
2014-05-04 20:41:15 -0300

    Added core test.

--- dashboardsus/apps/core/tests.py
+++ dashboardsus/apps/core/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+rom django.test import TestCase, Client
+from dashboardsus.apps.accounts.models import CustomUser
+from dashboardsus.apps.core.models import Municipio
+
+class CoreTests(TestCase):
+    def setUp(self):
+        user = CustomUser.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+    def test_municipio_can_be_created_with_no_parameters(self):
+        Municipio.objects.create()
+        self.assertEqual(Municipio.objects.count(), 1)
+
+    def test_home_route(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 301)
 
 # Create your tests here.
